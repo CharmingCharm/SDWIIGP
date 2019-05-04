bdcf95603d6f4ded67b8f3ded53a11796c7e3be8
mchobbylong <mchobbylong@gmail.com>
2019-05-05 03:03:02 +0800

    add new shell command to add users
    usage: python manage.py adduser root mypassword true

--- backend/manage.py
+++ backend/manage.py
@@ -1,6 +1,8 @@
 from app import create_app
 from flask_script import Manager, Server
 from flask_migrate import MigrateCommand
+from app.extension import db
+from app.model import User, Problem
 import os
 
 config_name = os.environ.get('FLASK_CONFIG') or 'default'
@@ -12,6 +14,12 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 manager.add_command('runserver', Server(use_debugger = True))
+
+@manager.command
+def adduser(name, pwd, teacher):
+    is_teacher = True if teacher == 'true' else False
+    db.session.add(User(user_name = name, password = pwd, is_teacher = is_teacher))
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
