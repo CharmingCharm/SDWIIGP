6faa807680cc9971038f8117477573a46134cfbf
mchobbylong <mchobbylong@gmail.com>
2019-05-05 03:32:08 +0800

    finish templates about problemset

--- backend/app/controller/problem.py
+++ backend/app/controller/problem.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, current_app, redirect, url_for, flash, request, render_template_string
 from flask_login import current_user, login_required
+from app.model import serialize, Problem, Tag
 
 from app.extension import db
 
@@ -8,4 +9,16 @@ problem = Blueprint('problem', __name__)
 @problem.route('/', methods = ['GET', 'POST'])
 @login_required
 def problemset():
-    return render_template('problem/problemset.html')
+    return render_template(
+        'problem/problemset.html',
+        problems = [serialize(prob) for prob in Problem.query.all()],
+        tags = [serialize(tag) for tag in Tag.query.all()]
+    )
+
+@problem.route('/<pid>', methods = ['GET', 'POST'])
+@login_required
+def show(pid):
+    problem = Problem.query.filter_by(pid = pid).first()
+    problem_dict = serialize(problem)
+    problem_dict['tags'] = [s for s in problem.tags.all()]
+    return render_template('problem/problem.html', problem = problem_dict)
