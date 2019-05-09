ba598db72a8e35086dafc38461be373f66f1f967
mchobbylong <mchobbylong@gmail.com>
2019-05-10 03:11:35 +0800

    use json package from flask in api

--- backend/app/controller/api.py
+++ backend/app/controller/api.py
@@ -1,8 +1,6 @@
-from flask import Blueprint, current_app, redirect, url_for, flash, request, render_template_string
+from flask import Blueprint, current_app, redirect, url_for, flash, request, json
 from flask_login import current_user, login_required
 from app.model import serialize
-from . import render_template
-import json
 
 from app.extension import db
 
@@ -10,4 +8,4 @@ api = Blueprint('api', __name__)
 
 @api.route('/get_user', methods = ['GET', 'POST'])
 def get_user():
-    return json.dumps(serialize(current_user))
+    return json.jsonify(serialize(current_user))
