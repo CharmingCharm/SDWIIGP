03e9ba9a07d98e971eb82e8c414078567e8b2a82
Charm Zhang <qiaomuzhangCharm@outlook.com>
2019-05-11 13:22:00 +0800

    - Add base_problem.html - Add task.html - Change the problem.html to calling base_problem.html - Add task_problem.html calling base_problem.html

--- backend/app/controller/task.py
+++ backend/app/controller/task.py
@@ -1,12 +1,22 @@
 from flask import Blueprint, current_app, redirect, url_for, flash, request, render_template_string
 from flask_login import current_user, login_required
-from app.model import serialize, Problem, Tag
+from app.model import serialize, Task
 from . import render_template
 from app.extension import db
+from datetime import datetime
 
 tasks = Blueprint('task', __name__)
 
 @tasks.route('/', methods = ['GET', 'POST'])
 @login_required
 def task():
-    return render_template('task.html')
+    tasks = [serialize(task) for task in Task.query.all()]
+    for task in tasks:
+        task['deadline'] = datetime.strptime(task['deadline'], '%Y-%m-%d %H:%M:%S')
+    return render_template('task.html',tasks = tasks,now = datetime.now())
+
+@tasks.route('/<task_id>', methods = ['GET', 'POST'])
+@login_required
+def show(task_id):
+    task = Task.query.filter_by(task_id = task_id).first()
+    return render_template('problem/task_problem.html', task = task)
