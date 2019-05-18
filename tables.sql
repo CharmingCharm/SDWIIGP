35f1a1b31c207c73fa0eab64cee35c473beaaab0
mchobbylong <mchobbylong@gmail.com>
2019-05-18 23:06:20 +0800

    update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,112 +1,278 @@
-/*
-drop database if exists oj;
+-- MySQL dump 10.15  Distrib 10.0.38-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: oj
+-- ------------------------------------------------------
+-- Server version	10.0.38-MariaDB-0ubuntu0.16.04.1
 
-create database oj;
-grant all privileges on oj.* to 'sdwii'@'%' identified by 'sdwii';
-flush privileges;
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-use oj;
+--
+-- Table structure for table `alembic_version`
+--
 
-create table UserGroup(
-	gID int not null auto_increment,
-	groupName varchar(64) not null,
-	description text not null default '',
-	primary key (gID)
-);
+DROP TABLE IF EXISTS `alembic_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table User(
-	uID int not null auto_increment,
-	name varchar(64) not null,
-	password varchar(256) not null,
-	isTeacher boolean not null default False,
-	primary key (uID),
-	unique key uq_User_name (name)
-);
+--
+-- Table structure for table `announcement`
+--
 
-create table Problem(
-	pID int not null auto_increment,
-	tsID int default null,
-	level int not null,
-	title varchar(64) not null,
-	description text not null,
-	primary key (pID)
-) auto_increment=1000;
+DROP TABLE IF EXISTS `announcement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `announcement` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) DEFAULT NULL,
+  `title` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  `date_time` datetime NOT NULL,
+  PRIMARY KEY (`aid`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `announcement_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table Submission(
-	sID int not null auto_increment,
-	uID int not null,
-	pID int not null,
-	score numeric(6,2) default null,
-	code text not null,
-	isSolution boolean not null default False,
-	datetime timestamp not null default CURRENT_TIMESTAMP,
-	primary key (sID),
-	constraint `fk_Submission_uID` foreign key (uID) references User (uID),
-	constraint `fk_Submission_pID` foreign key (pID) references Problem (pID)
-);
+--
+-- Table structure for table `problem`
+--
 
-create table Tag(
-	tagID int not null auto_increment,
-	tagName varchar(64) not null,
-	primary key (tagID)
-);
+DROP TABLE IF EXISTS `problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT,
+  `level` int(11) NOT NULL,
+  `title` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  `testset_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pid`),
+  KEY `testset_id` (`testset_id`),
+  CONSTRAINT `problem_ibfk_1` FOREIGN KEY (`testset_id`) REFERENCES `testset` (`testset_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table Task(
-	taskID int not null auto_increment,
-	taskName varchar(64) not null,
-	deadline timestamp not null,
-	primary key (taskID)
-);
+--
+-- Table structure for table `problem_in_task`
+--
 
-create table TestSet(
-	tsID int not null auto_increment,
-	pID int not null,
-	primary key (tsID),
-	constraint `fk_TestSet_pID` foreign key (pID) references Problem (pID)
-);
+DROP TABLE IF EXISTS `problem_in_task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `problem_in_task` (
+  `task_id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  PRIMARY KEY (`task_id`,`pid`),
+  KEY `pid` (`pid`),
+  CONSTRAINT `problem_in_task_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `problem` (`pid`),
+  CONSTRAINT `problem_in_task_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table Test(
-	testID int not null auto_increment,
-	score numeric(6,2) not null,
-	code text not null,
-	primary key (testID)
-);
+--
+-- Table structure for table `submission`
+--
 
-create table Announcement(
-	aID int not null auto_increment,
-	uID int not null,
-	title varchar(64) not null,
-	description text not null default '',
-	datetime timestamp not null default CURRENT_TIMESTAMP,
-	primary key (aID),
-	constraint `fk_Announcement_uID` foreign key (uID) references User (uID)
-);
+DROP TABLE IF EXISTS `submission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `submission` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) DEFAULT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `score` text,
+  `code` text NOT NULL,
+  `is_solution` tinyint(1) NOT NULL,
+  `date_time` datetime NOT NULL,
+  PRIMARY KEY (`sid`),
+  KEY `pid` (`pid`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `submission_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `problem` (`pid`),
+  CONSTRAINT `submission_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table UserInGroup(
-	uID int not null,
-	gID int not null,
-	constraint `fk_UserInGroup_uID` foreign key (uID) references User (uID),
-	constraint `fk_UserInGroup_gID` foreign key (gID) references UserGroup (gID)
-);
+--
+-- Table structure for table `tag`
+--
 
-create table TagOfProblem(
-	tagID int not null,
-	pID int not null,
-	constraint `fk_TagOfProblem_tagID` foreign key (tagID) references Tag (tagID),
-	constraint `fk_TagOfProblem_pID` foreign key (pID) references Problem (pID)
-);
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `tag_id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(64) NOT NULL,
+  PRIMARY KEY (`tag_id`),
+  UNIQUE KEY `tag_name` (`tag_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table TestSetHasTest(
-	tsID int not null,
-	testID int not null,
-	constraint `fk_TestSetHasTest_tsID` foreign key (tsID) references TestSet (tsID),
-	constraint `fk_TestSetHasTest_testID` foreign key (testID) references Test (testID)
-);
+--
+-- Table structure for table `tag_of_problem`
+--
 
-create table ProblemInTask(
-	taskID int not null,
-	pID int not null,
-	constraint `fk_ProblemInTask_taskID` foreign key (taskID) references Task (taskID),
-	constraint `fk_ProblemInTask_pID` foreign key (pID) references Problem (pID)
-);
+DROP TABLE IF EXISTS `tag_of_problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag_of_problem` (
+  `tag_id` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  PRIMARY KEY (`tag_id`,`pid`),
+  KEY `pid` (`pid`),
+  CONSTRAINT `tag_of_problem_ibfk_1` FOREIGN KEY (`pid`) REFERENCES `problem` (`pid`),
+  CONSTRAINT `tag_of_problem_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_name` varchar(64) NOT NULL,
+  `deadline` datetime NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `task_for_user_group`
+--
+
+DROP TABLE IF EXISTS `task_for_user_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `task_for_user_group` (
+  `gid` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  PRIMARY KEY (`gid`,`task_id`),
+  KEY `task_id` (`task_id`),
+  CONSTRAINT `task_for_user_group_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `user_group` (`gid`),
+  CONSTRAINT `task_for_user_group_ibfk_2` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` (
+  `test_id` int(11) NOT NULL AUTO_INCREMENT,
+  `score` decimal(6,2) NOT NULL,
+  `code` text NOT NULL,
+  PRIMARY KEY (`test_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test_in_testset`
+--
+
+DROP TABLE IF EXISTS `test_in_testset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test_in_testset` (
+  `testset_id` int(11) NOT NULL,
+  `test_id` int(11) NOT NULL,
+  PRIMARY KEY (`testset_id`,`test_id`),
+  KEY `test_id` (`test_id`),
+  CONSTRAINT `test_in_testset_ibfk_1` FOREIGN KEY (`test_id`) REFERENCES `test` (`test_id`),
+  CONSTRAINT `test_in_testset_ibfk_2` FOREIGN KEY (`testset_id`) REFERENCES `testset` (`testset_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `testset`
+--
+
+DROP TABLE IF EXISTS `testset`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `testset` (
+  `testset_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`testset_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) NOT NULL,
+  `password_hash` varchar(256) NOT NULL,
+  `is_teacher` tinyint(1) NOT NULL,
+  `photo` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `user_name` (`user_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_group`
+--
+
+DROP TABLE IF EXISTS `user_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_group` (
+  `gid` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(64) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`gid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_in_group`
+--
+
+DROP TABLE IF EXISTS `user_in_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_in_group` (
+  `uid` int(11) NOT NULL,
+  `gid` int(11) NOT NULL,
+  PRIMARY KEY (`uid`,`gid`),
+  KEY `gid` (`gid`),
+  CONSTRAINT `user_in_group_ibfk_1` FOREIGN KEY (`gid`) REFERENCES `user_group` (`gid`),
+  CONSTRAINT `user_in_group_ibfk_2` FOREIGN KEY (`uid`) REFERENCES `user` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-05-18 17:44:29
