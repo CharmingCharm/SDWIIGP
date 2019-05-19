4faf30901ddf012085b48349fe2a537125aa5b36
mchobbylong <mchobbylong@gmail.com>
2019-05-20 02:10:47 +0800

    remove chinese characters

--- backend/app/helper.py
+++ backend/app/helper.py
@@ -1,7 +1,5 @@
 import random
 
-
-# 生成随机的字符串
 def random_string(length=32):
-    base_str = 'abcdefghijklnopqrstuvwxyz1234567890'
-    return ''.join(random.choice(base_str) for i in range(length))
+	base_str = 'abcdefghijklnopqrstuvwxyz1234567890'
+	return ''.join(random.choice(base_str) for i in range(length))
