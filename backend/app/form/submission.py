804f36683cb1b50e2d6aa47616987d77ef2867ed
Michael Long <mchobbylong@gmail.com>
2019-05-23 01:02:36 +0800

    remove unnecessary validators in FormRejudge

--- backend/app/form/submission.py
+++ backend/app/form/submission.py
@@ -8,9 +8,5 @@ class FormRejudge(FlaskForm):
 	sid = IntegerField('Submission ID')
 	pid = IntegerField('Problem ID')
 	task_id = IntegerField('Task ID')
-	rejudge_type = SelectField(
-		'Rejudge Type',
-		choices = [('pending', 'pending'), ('sid', 'sid'), ('pid', 'pid'), ('task_id', 'task_id')],
-		validators = [DataRequired('Rejudge type is required!')]
-	)
+	rejudge_type = StringField('Rejudge Type')
 	rejudge_confirm = StringField('Confirm')
