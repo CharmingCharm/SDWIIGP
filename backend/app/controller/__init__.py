6c13a8328c66780ab8b89fb4006afcb865ac24a0
mchobbylong <mchobbylong@gmail.com>
2019-05-26 21:59:12 +0800

    remove blueprint api

--- backend/app/controller/__init__.py
+++ backend/app/controller/__init__.py
@@ -21,7 +21,6 @@ def str_time(dt):
 
 from .main import main
 from .user import user
-from .api import api
 from .task import task
 from .admin import admin
 from .problem import problem
@@ -30,7 +29,6 @@ from .submission import submission
 BLUEPRINTS = (
 	(main, ''),
 	(user, '/user'),
-	(api, '/api'),
 	(problem, '/problem'),
 	(task, '/task'),
 	(admin, '/admin'),
